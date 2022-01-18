f765d74489ce1e6338b1d4cbcef818b460ae0732
Stilic <lateman-jpeg@outlook.fr>
2022-01-18 20:15:00 +0100

    what the comment

--- source/meta/data/dependency/FNFUIState.hx
+++ source/meta/data/dependency/FNFUIState.hx
@@ -8,7 +8,7 @@ class FNFUIState extends FlxUIState
 	override function create()
 	{
 		// state stuffs
-		// just noticed i had to add 0.3s for make it act like trans in
+		// just noticed i had to add 0.4s for make it act like trans in
 		if (!FlxTransitionableState.skipNextTransOut)
 			openSubState(new FNFTransition(1.1, true));
 	
