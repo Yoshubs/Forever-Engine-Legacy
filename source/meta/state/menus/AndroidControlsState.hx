0cf0ad309e8427d1836cceede4f8bba83d8c3e67
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-01-24 19:50:15 +0200

    I'm stupid

--- source/meta/state/menus/AndroidControlsState.hx
+++ source/meta/state/menus/AndroidControlsState.hx
@@ -69,7 +69,7 @@ class AndroidControlsState extends MusicBeatState
 		inputvari = new FlxText(125, 50, 0,controlitems[0], 48);
 		
 		//arrows
-		var ui_tex = Paths.getSparrowAtlas('menus/base/storymenu/campaign_menu_UI_assets);
+		var ui_tex = Paths.getSparrowAtlas('menus/base/storymenu/campaign_menu_UI_assets');
 
 		leftArrow = new FlxSprite(inputvari.x - 60,inputvari.y - 10);
 		leftArrow.frames = ui_tex;
