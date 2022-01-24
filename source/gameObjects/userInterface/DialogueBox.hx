b9f7af3428ff9f5a0bdd7dd417c00095d045144d
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-01-24 15:44:16 +0200

    Ok

--- source/gameObjects/userInterface/DialogueBox.hx
+++ source/gameObjects/userInterface/DialogueBox.hx
@@ -103,7 +103,7 @@ class DialogueBox extends FlxSpriteGroup
 		var truePath = Paths.file(file);
 
 		// load the json file
-		if (sys.FileSystem.exists(dialoguePath))
+		if (sys.FileSystem.exists(Main.getDataPath() + dialoguePath))
 			return dialoguePath;
 		else
 			return truePath;
@@ -230,7 +230,7 @@ class DialogueBox extends FlxSpriteGroup
 			var boxJson = Paths.file('images/dialogue/boxes/$curBox/$curBox.json');
 
 			// load the json and sprite
-			boxData = haxe.Json.parse(sys.io.File.getContent(boxJson));
+			boxData = haxe.Json.parse(sys.io.File.getContent(Main.getDataPath() + boxJson));
 			box.frames = Paths.getSparrowAtlas('dialogue/boxes/$curBox/$curBox');
 
 			// get the states sectioon
