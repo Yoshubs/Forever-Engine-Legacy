c06940c4efb5537d4543a92ade4c9efc79ac0c30
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-01-24 21:22:57 +0200

    The thing

--- source/meta/subState/OptionsSubstate.hx
+++ source/meta/subState/OptionsSubstate.hx
@@ -95,6 +95,10 @@ class OptionsSubstate extends MusicBeatSubState
 		submenu.visible = false;
 		submenuGroup.visible = false;
 		submenuoffsetGroup.visible = false;
+
+                #if android
+		addVirtualPad(FULL, A_B);
+                #end
 	}
 
 	private var keyOptions:FlxTypedGroup<Alphabet>;
