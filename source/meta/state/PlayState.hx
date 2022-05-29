afac822a2ede73575b7f77a484ed7554d317a11b
jigsaw-4277821 <mihaialexandru427@yahoo.com>
2022-05-29 16:00:37 +0300

    aaaaaaaa

--- source/meta/state/PlayState.hx
+++ source/meta/state/PlayState.hx
@@ -730,7 +730,7 @@ class PlayState extends MusicBeatState
 
 	function controllerInput()
 	{
-		if (!strumline.autoplay) {
+		if (!strumLines.autoplay) {
 			var controlArray:Array<Bool> = [controls.LEFT_P, controls.DOWN_P, controls.UP_P, controls.RIGHT_P];
 			if(controlArray.contains(true))
 			{
