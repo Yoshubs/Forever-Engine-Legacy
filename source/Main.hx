3772b951c5960b3bf549bdd325a8661201a9d958
BeastlyGhost <45212377+BeastlyGhost@users.noreply.github.com>
2022-06-18 16:35:18 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -99,7 +99,7 @@ class Main extends Sprite
 	public static var mainClassState:Class<FlxState> = Init; // Determine the main class state of the game
 	public static var framerate:Int = 120; // How many frames per second the game should run at.
 
-	public static var gameVersion:String = '0.3_01'; //anything before "0.3" is the fork version
+	public static var gameVersion:String = '0.3_01'; //anything after "0.3" is the fork version
 
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
