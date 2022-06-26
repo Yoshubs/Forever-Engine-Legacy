d518fa839bcbf606da695ed5887454da18a0ffdb
BeastlyGhost <45212377+BeastlyGhost@users.noreply.github.com>
2022-06-26 15:23:29 -0400

    blah

--- setup.bat
+++ setup.bat
@@ -1,22 +1,24 @@
 @echo off
 title FNF Setup - Start
-echo Make sure Haxe 4.1.5 and HaxeFlixel is installed (4.1.5 is important)!
 echo Press any key to install required libraries.
 pause >nul
 title FNF Setup - Installing libraries
 echo Installing haxelib libraries...
-haxelib install lime 7.8.0
+haxelib install lime
 haxelib install openfl
-haxelib install flixel 4.8.1
-haxelib install flixel-addons
+haxelib install flixel
 haxelib install flixel-ui
+haxelib install flixel-tools
 haxelib install hscript
 haxelib install newgrounds
-haxelib run lime setup
-haxelib install flixel-tools
+haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
+haxelib git hxCodec https://github.com/polybiusproxy/hxCodec
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 title FNF Setup - User action required
 cls
 haxelib run flixel-tools setup
+haxelib run lime setup flixel
 cls
 echo Make sure you have git installed. You can download it here: https://git-scm.com/downloads
 echo Press any key to install polymod.
@@ -29,43 +31,9 @@ pause >nul
 title FNF Setup - Installing libraries
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 cls
-goto UserActions1
+goto CommunitySetup
 
-:UserActions1
-title FNF Setup - User action required
-set /p menu="Would you like to fix the transition bug? [Y/N]"
-       if %menu%==Y goto FixTransitionBug
-       if %menu%==y goto FixTransitionBug
-       if %menu%==N goto UserActions2
-       if %menu%==n goto UserActions2
-       cls
-
-:UserActions2
-cls
-title FNF Setup - User action required
-set /p menu2="Would you like to automatically make the APIStuff file? [Y/N]"
-       if %menu2%==Y goto APIStuffYes
-       if %menu2%==y goto APIStuffYes
-       if %menu2%==N goto APIStuffNo
-       if %menu2%==n goto APIStuffNo
-       cls
-       
-:APIStuffYes
-rem Stores the APIStuff.hx contents automatically
-cd source
-(
-echo package;
-echo class APIStuff
-echo {
-echo         public static var API:String = "";
-echo         public static var EncKey:String = "";
-echo }
-)>APIStuff.hx
-cd ..
-goto APIStuffNo
-
-
-:APIStuffNo
+:CommunitySetup
 cls
 title FNF Setup - User action required
 set /p menu="Would you like to install Visual Studio Community and components? (Necessary to compile/ 5.5GB) [Y/N]"
@@ -82,11 +50,6 @@ title FNF Setup - Success
 echo Setup successful. Press any key to exit.
 pause >nul
 exit
-
-:FixTransitionBug
-title FNF Setup - Installing libraries
-haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
-goto UserActions2
 
 :InstallVSCommunity
 title FNF Setup - Installing Visual Studio Community
