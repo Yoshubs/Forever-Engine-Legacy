2f9e130400dfb07f21c072d7be8e40a887199065
BeastlyGhost <45212377+BeastlyGhost@users.noreply.github.com>
2022-06-29 13:19:04 -0400

    blah

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1964,7 +1964,7 @@ class PlayState extends MusicBeatState
 
 	public function startVideo(name:String, loop:Bool = false, haccelerated:Bool = true, pauseMusic:Bool = false)
 	{
-		/*#if VIDEO_PLUGIN
+		#if VIDEO_PLUGIN
 		inCutscene = true;
 
 		var filepath:String = Paths.video(name);
@@ -1997,7 +1997,7 @@ class PlayState extends MusicBeatState
 		FlxG.log.warn('Platform not supported!');
 		startAndEnd();
 		return;
-		#end*/
+		#end
 	}
 
 	function startAndEnd()
