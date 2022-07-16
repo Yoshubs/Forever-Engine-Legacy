630056a89a276582de7c7f4d07408263a0f52acf
BeastlyGhost <45212377+BeastlyGhost@users.noreply.github.com>
2022-07-16 03:12:51 -0400

    Update CharacterDebug.hx

--- source/states/CharacterDebug.hx
+++ source/states/CharacterDebug.hx
@@ -1,11 +1,14 @@
 package states;
 
+import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.addons.transition.FlxTransitionableState;
-import flixel.addons.transition.TransitionData;
+import flixel.addons.ui.FlxUI;
+import flixel.addons.ui.FlxUICheckBox;
+import flixel.addons.ui.FlxUIDropDownMenu;
+import flixel.addons.ui.FlxUITabMenu;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
@@ -19,9 +22,12 @@ import openfl.net.FileReference;
 using StringTools;
 
 /**
-	*DEBUG MODE
+	character offset editor
+	this is just code from the base game
+	with some tweaking here and there to make it work on forever engine
+	and some other additional features
  */
-
+ 
 class CharacterDebug extends FlxState
 {
 	var _file:FileReference;
@@ -33,34 +39,48 @@ class CharacterDebug extends FlxState
 
 	var isDad:Bool = true;
 	var curCharacter:String = 'dad';
-	
+
+	var curAnim:Int = 0;
+
 	var textAnim:FlxText;
 	var dumbTexts:FlxTypedGroup<FlxText>;
-	var animList:Array<String> = [];
-	var curAnim:Int = 0;
+	var animList:Array<String> = [''];
+
 	var camFollow:FlxObject;
+
+	var ghost:Character;
 
 	private var stageBuild:Stage;
 	var curStage:String = 'stage';
 
-	// transitions
-	var transOut:TransitionData;
-	var transIn:TransitionData;
+	private var camGame:FlxCamera;
+	private var camHUD:FlxCamera;
+
+	var UI_box:FlxUITabMenu;
+
+	var canOffset:Bool = false;
+
+	var fileExt:String = 'hxs';
 
 	public function new(curCharacter:String = 'dad', curStage:String = 'stage')
 	{
 		super();
 		this.curCharacter = curCharacter;
-        this.curStage = curStage;
+		this.curStage = curStage;
 	}
 
 	override function create()
 	{
-		// set the transitions to the previously set ones
-		transIn = FlxTransitionableState.defaultTransIn;
-		transOut = FlxTransitionableState.defaultTransOut;
-
 		FlxG.sound.music.stop();
+
+		FlxG.mouse.visible = true;
+
+		//camGame = new FlxCamera();
+		camHUD = new FlxCamera();
+		camHUD.bgColor.alpha = 0;
+
+		//FlxG.cameras.reset(camGame);
+		//FlxG.cameras.add(camHUD);
 
 		stageBuild = new Stage(curStage);
 		add(stageBuild);
@@ -68,9 +88,14 @@ class CharacterDebug extends FlxState
 		if (curCharacter.startsWith('bf'))
 			isDad = false;
 
+		ghost = new Character(0, 0, curCharacter);
+		ghost.debugMode = true;
+		ghost.visible = false;
+		add(ghost);
+
 		if (isDad)
 		{
-			dadOpponent = new Character(100, 100, curCharacter);
+			dadOpponent = new Character(0, 0, curCharacter);
 			dadOpponent.screenCenter();
 			dadOpponent.debugMode = true;
 			add(dadOpponent);
@@ -80,7 +105,7 @@ class CharacterDebug extends FlxState
 		}
 		else
 		{
-			bf = new Boyfriend(770, 450);
+			bf = new Boyfriend(0, 0);
 			bf.screenCenter();
 			bf.debugMode = true;
 			add(bf);
@@ -98,7 +123,40 @@ class CharacterDebug extends FlxState
 		textAnim.scrollFactor.set();
 		add(textAnim);
 
-		generateCharacterOffsets();
+		genCharOffsets();
+
+		var tipTextArray:Array<String> = "E/Q - Camera Zoom In/Out
+		\nR - Reset Camera Zoom
+		\nJKLI - Move Camera
+		\nW/S - Previous/Next Animation
+		\nSpace - Play Animation
+		\nArrow Keys - Move Character Offset
+		\nF - Flip Character horizontally
+		\nHold Shift to Move 10x faster\n".split('\n');
+
+		for (i in 0...tipTextArray.length - 1)
+		{
+			var tipText:FlxText = new FlxText(FlxG.width - 320, FlxG.height - 15 - 16 * (tipTextArray.length - i), 300, tipTextArray[i], 12);
+			//tipText.cameras = [camHUD];
+			tipText.setFormat(null, 12, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE_FAST, FlxColor.BLACK);
+			tipText.scrollFactor.set();
+			tipText.borderSize = 1.5;
+			add(tipText);
+		}
+
+		var tabs = [
+			{name: 'Character', label: 'Character'},
+		];
+
+		UI_box = new FlxUITabMenu(null, tabs, true);
+		//UI_box.cameras = [camHUD];
+		UI_box.resize(250, 120);
+		UI_box.x = FlxG.width - 275;
+		UI_box.y = 25;
+		UI_box.scrollFactor.set();
+		add(UI_box);
+
+		addCharacterUI();
 
 		camFollow = new FlxObject(0, 0, 2, 2);
 		camFollow.screenCenter();
@@ -109,25 +167,60 @@ class CharacterDebug extends FlxState
 		super.create();
 	}
 
-	function generateCharacterOffsets(pushList:Bool = true):Void
+	var ghostAnimDropDown:FlxUIDropDownMenu;
+	function addCharacterUI()
+	{
+		var tab_group = new FlxUI(null, UI_box);
+		tab_group.name = "Character";
+
+		var check_offset = new FlxUICheckBox(10, 60, null, null, "Offset Mode", 100);
+		check_offset.callback = function()
+		{
+			canOffset = !canOffset;
+		};
+		check_offset.checked = true;
+
+		ghostAnimDropDown = new FlxUIDropDownMenu(10, 30, FlxUIDropDownMenu.makeStrIdLabelArray(animList, true), function(animation:String)
+		{
+			if (animList[0] != '' || animList[0] != null)
+			{
+				ghost.visible = true;
+				ghost.alpha = 0.85;
+				ghost.playAnim(animList[0], true);
+			}
+			else
+			{
+				ghost.visible = false;
+			}
+		});
+
+		tab_group.add(new FlxText(ghostAnimDropDown.x, ghostAnimDropDown.y - 18, 0, 'Ghost Animation:'));
+		tab_group.add(check_offset);
+		tab_group.add(ghostAnimDropDown);
+		UI_box.addGroup(tab_group);
+	}
+
+	function genCharOffsets(pushList:Bool = true):Void
 	{
 		var daLoop:Int = 0;
 
 		for (anim => offsets in char.animOffsets)
 		{
 			var text:FlxText = new FlxText(10, 20 + (18 * daLoop), 0, anim + ": " + offsets, 15);
+			text.setFormat(Paths.font('vcr.ttf'), 18, FlxColor.WHITE);
+			text.setBorderStyle(OUTLINE, FlxColor.BLACK, 1.5);
 			text.scrollFactor.set();
-			text.color = FlxColor.BLUE;
 			dumbTexts.add(text);
 
 			if (pushList)
 				animList.push(anim);
 
+			char.setPosition(offsets[0], offsets[1]);
 			daLoop++;
 		}
 	}
 
-	function saveCharacterOffsets():Void
+	function saveCharOffsets():Void
 	{
 		var result = "";
 
@@ -143,7 +236,7 @@ class CharacterDebug extends FlxState
 			_file.addEventListener(Event.COMPLETE, onSaveComplete);
 			_file.addEventListener(Event.CANCEL, onSaveCancel);
 			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
-			_file.save(result.trim(), curCharacter + "Offsets.hxs");
+			_file.save(result.trim(), curCharacter + "Offsets." + fileExt);
 		}
 	}
 
@@ -194,61 +287,66 @@ class CharacterDebug extends FlxState
 	override function update(elapsed:Float)
 	{
 		textAnim.text = char.animation.curAnim.name;
+		ghost.flipX = char.flipX;
 
-		if (FlxG.keys.justPressed.ENTER || FlxG.keys.justPressed.ESCAPE)
+		if (FlxG.keys.justPressed.ENTER || FlxG.keys.justPressed.ESCAPE) {
+			FlxG.mouse.visible = false;
 			Main.switchState(this, new PlayState());
+		}
 
-		if (FlxG.keys.justPressed.E)
-			FlxG.camera.zoom += 0.25;
-		if (FlxG.keys.justPressed.Q)
-			FlxG.camera.zoom -= 0.25;
+		if (FlxG.keys.justPressed.R)
+		{
+			FlxG.camera.zoom = 1;
+		}
+
+		if (FlxG.keys.pressed.E && FlxG.camera.zoom < 3)
+		{
+			FlxG.camera.zoom += elapsed * FlxG.camera.zoom;
+			if (FlxG.camera.zoom > 3)
+				FlxG.camera.zoom = 3;
+		}
+		if (FlxG.keys.pressed.Q && FlxG.camera.zoom > 0.1)
+		{
+			FlxG.camera.zoom -= elapsed * FlxG.camera.zoom;
+			if (FlxG.camera.zoom < 0.1)
+				FlxG.camera.zoom = 0.1;
+		}
 
 		if (FlxG.keys.justPressed.F)
 			char.flipX = !char.flipX;
 
 		if (FlxG.keys.pressed.I || FlxG.keys.pressed.J || FlxG.keys.pressed.K || FlxG.keys.pressed.L)
 		{
+			var addToCam:Float = 500 * elapsed;
+			if (FlxG.keys.pressed.SHIFT)
+				addToCam *= 4;
+
 			if (FlxG.keys.pressed.I)
-				camFollow.velocity.y = -190;
+				camFollow.y -= addToCam;
 			else if (FlxG.keys.pressed.K)
-				camFollow.velocity.y = 190;
-			else
-				camFollow.velocity.y = 0;
+				camFollow.y += addToCam;
 
 			if (FlxG.keys.pressed.J)
-				camFollow.velocity.x = -190;
+				camFollow.x -= addToCam;
 			else if (FlxG.keys.pressed.L)
-				camFollow.velocity.x = 190;
-			else
-				camFollow.velocity.x = 0;
-		}
-		else
-		{
-			camFollow.velocity.set();
+				camFollow.x += addToCam;
 		}
 
-		if (FlxG.keys.justPressed.W)
+		if (canOffset)
 		{
-			curAnim -= 1;
-		}
+			if (FlxG.keys.justPressed.W)
+				updateAnimation(-1);
+			else if (FlxG.keys.justPressed.S)
+				updateAnimation(1);
 
-		if (FlxG.keys.justPressed.S)
-		{
-			curAnim += 1;
-		}
+			if (FlxG.keys.justPressed.S || FlxG.keys.justPressed.W || FlxG.keys.justPressed.SPACE)
+			{
+				char.playAnim(animList[curAnim]);
+				ghost.playAnim(animList[curAnim]);
 
-		if (curAnim < 0)
-			curAnim = animList.length - 1;
-
-		if (curAnim >= animList.length)
-			curAnim = 0;
-
-		if (FlxG.keys.justPressed.S || FlxG.keys.justPressed.W || FlxG.keys.justPressed.SPACE)
-		{
-			char.playAnim(animList[curAnim]);
-
-			updateTexts();
-			generateCharacterOffsets(false);
+				updateTexts();
+				genCharOffsets(false);
+			}
 		}
 
 		var upP = FlxG.keys.anyJustPressed([UP]);
@@ -264,23 +362,40 @@ class CharacterDebug extends FlxState
 		if (upP || rightP || downP || leftP)
 		{
 			updateTexts();
-			if (upP)
+			if (upP) {
 				char.animOffsets.get(animList[curAnim])[1] += 1 * multiplier;
-			if (downP)
+			}
+			if (downP) {
 				char.animOffsets.get(animList[curAnim])[1] -= 1 * multiplier;
-			if (leftP)
+			}
+			if (leftP) {
 				char.animOffsets.get(animList[curAnim])[0] += 1 * multiplier;
-			if (rightP)
+			}
+			if (rightP) {
 				char.animOffsets.get(animList[curAnim])[0] -= 1 * multiplier;
+			}
 
 			updateTexts();
-			generateCharacterOffsets(false);
+			genCharOffsets(false);
+
+			ghost.setPosition(char.x, char.y);
 			char.playAnim(animList[curAnim]);
+			ghost.playAnim(animList[curAnim]);
 		}
 
 		if (FlxG.keys.pressed.CONTROL && FlxG.keys.justPressed.S)
-			saveCharacterOffsets();
+			saveCharOffsets();
 
 		super.update(elapsed);
+	}
+
+	function updateAnimation(hey:Int)
+	{
+		curAnim += hey;
+
+		if (curAnim < 0)
+			curAnim = animList.length;
+		if (curAnim >= animList.length)
+			curAnim = 0;
 	}
 }
